c5fde81d0093c687784725ed530612f52fcb8345
shozawa <t.shozawa@mugenup.com>
2017-04-26 09:45:06 +0900

    Rails5 API compatible

--- lib/sorcery/engine.rb
+++ lib/sorcery/engine.rb
@@ -7,10 +7,16 @@ module Sorcery
   class Engine < Rails::Engine
     config.sorcery = ::Sorcery::Controller::Config
 
-    initializer 'extend Controller with sorcery' do
-      ActionController::Base.send(:include, Sorcery::Controller)
-      ActionController::Base.helper_method :current_user
-      ActionController::Base.helper_method :logged_in?
+    initializer 'extend Controller with sorcery' do |app|
+      if app.config.api_only
+        ActionController::API.send(:include, Sorcery::Controller)
+        ActionController::API.send(:include, ActionController::Cookies)
+        app.config.middleware.use ActionDispatch::Cookies
+      else
+        ActionController::Base.send(:include, Sorcery::Controller)
+        ActionController::Base.helper_method :current_user
+        ActionController::Base.helper_method :logged_in?
+      end
     end
   end
 end
